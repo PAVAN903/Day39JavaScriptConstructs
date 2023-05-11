0e1fb76f965ba6b640201596a016983bcc32633f
PAVAN903 <navap903@gmail.com>
2023-05-11 11:26:47 +0530

    Added arrays and map programs using javascript

---
+++ Array/PrimeFactor.js
@@ -0,0 +1,10 @@
+let primefactorArray = new Array();let number = 20;
+for(let i = 2; i < number; i++) {
+    while(number%i == 0) {
+        console.log(i+" ");
+        number = number / i;
+    }
+    if(number > 2) {
+        console.log(number);
+    }
+}
